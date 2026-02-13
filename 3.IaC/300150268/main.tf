c3fedab36142d5495b31de39a19d93215ee07b72
Kemiche06 <mdsaid.kemiche@outlook.com>
2026-02-13 09:33:36 -0800

    hello 3andek lhalib

---
+++ 3.IaC/300150268/main.tf
@@ -0,0 +1,36 @@
+resource "proxmox_vm_qemu" "vm1" {
+
+  name        = var.pm_vm_name
+  target_node = "labinfo"
+  clone       = "ubuntu-jammy-template"
+  full_clone  = false
+
+  cores   = 2
+  sockets = 1
+  memory  = 2048
+
+  scsihw = "virtio-scsi-pci"
+
+  disk {
+    size    = "10G"
+    type    = "scsi"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  os_type    = "cloud-init"
+  ipconfig0  = var.pm_ipconfig0
+  nameserver = var.pm_nameserver
+
+  ciuser = "ubuntu"
+
+  sshkeys = <<EOF
+${file("C:/Users/PC/.ssh/id_rsa.pub")}
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD2pLhMqFGKffSdYvNCMAyM7598oBY+m/3q5AMXmb7IE6vq42+yGzqEUzZu9WrFckFD4Hq52rIU5DeOvi83DCF3uroXjNTEtCKdi+tY7cV18bHmsDsBHMqTnpuvroofgFWA0Pi++b2kGW2I5eyy1Qjv5rOp7y11Xe6XeZFEz7qQO1/xNiBMJEruG9Xldgooe4hkaOF39qnbqD4ui3LxYaTUTEulstw4wN70dSB8Zu9YQP7A7KU2zIEwJ1aw8whfO1CAM/AVvoDyqMtV8VXoaZSHOBgluMtinQfyyt473S2ZZeJlnmhK0F1gdOhO4SVZNRMj96m30ryYkYBFWvvLRP5N b300098957@ramena
+EOF
+
+}
